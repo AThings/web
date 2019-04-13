873c5748762ca328ac18c4f6a5319f3b381473d0
AThings <2846965655@qq.com>
2019-04-13 23:26:48 +0800

    2019-4-13

---
+++ vue/my-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <fengjialue lp="fengjialue"/>
+    
+  </div>
+</template>
+
+<script>
+import fengjialue from './components/fengjialue.vue'
+import Vue from 'vue/dist/vue.esm.js'
+
+
+export default {
+  name: 'app',
+  components: {
+    fengjialue
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/my-project/src/components/fengjialue.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="test">
+    <h1>Hello every one,I Like {{ lp }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FengJiaLue',
+  props: {
+    lp: String
+  }
+}
+</script>
