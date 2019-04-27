23dfb7f662392e38ae417dfd94f05b58f1bf1191
AThings <2846965655@qq.com>
2019-04-27 11:21:36 +0800

    20190427

--- vue/vue_app/src/views/Movie/Movie.vue
+++ vue/vue_app/src/views/Movie/Movie.vue
@@ -2,9 +2,7 @@
     <div>
         <ul>
             <li v-for="(item,index) of list" :key="index" class="movieView">
-                <div class="content" @on-load="getimage(item.images.small)">
-                    <canvas></canvas>
-                    <!-- <img :src="item.images.small" alt="" crossOrigin = "Anonymous"> -->
+                <div class="content">
                     {{ item.title }}
                 </div>
             </li>
@@ -16,8 +14,8 @@
 export default {
     created(){
         this.$emit('routerChange','movie','电影');
-        console.log('movie');
         this.getData();
+        
     },
     data(){
         return {
@@ -28,34 +26,22 @@ export default {
     methods:{
         getData(){
             axios.get(API_PROXY+'https://api.douban.com/v2/movie/in_theaters').then((response)=>{
-                console.log(response)
+                console.log(response);
                 this.list = response.data.subjects;
             })
             .catch((error)=>{
-                console.log(error)
+                
             })
             .then(()=>{
-
+                this.setImages();
             });
         },
-        getimage(url){
-            console.log("1");
-            var imgs = new Image();
-            imgs.crossOrigin = "Anonymous"; //注意存放顺序
-            imgs.src = ""+url; 
-            imgs.onload = function () {
-                var canvas = document.createElement('canvas');
-                canvas.width = imgs.width;
-                canvas.height = imgs.height;
-                var ctx = canvas.getContext("2d");
-                ctx.drawImage(imgs, 0, 0, imgs.width, imgs.height);
-                var img1 = new Image();
-                img1.src = canvas.toDataURL("image/png");
-                alert("s")
-            }
-
-            imgs.onerror = function () {
-                alert("error")
+        setImages(){
+            var box = document.getElementsByClassName('content');
+            for(var i=0;i<this.list.length;i++){
+                var img = document.createElement('img');
+                img.src=this.list[i].images.small;
+                box[i].append(img);
             }
         }
     }
