e90ba20eaba36a0b2e38cd0fc7155c6d0a6cbaf3
AThings <2846965655@qq.com>
2019-05-03 21:15:51 +0800

    20190503

--- vue/vue_app/src/views/Movie/Movie.vue
+++ vue/vue_app/src/views/Movie/Movie.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <ul>
+            <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1556457796267&di=5f5c43ccff098662420182f72798e550&imgtype=0&src=http%3A%2F%2Fpic31.nipic.com%2F20130804%2F7487939_090818211000_2.jpg" alt="">
             <li v-for="(item,index) of list" :key="index" class="movieView">
                 <div class="content">
                     {{ item.title }}
